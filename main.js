7bdf0ac8ad6592d58ebc4d8a6b21f1361d02d189
atokad5 <kenny.krosky@gmail.com>
2019-07-24 18:13:19 -0700

    added error handling

--- main.js
+++ main.js
@@ -1,36 +1,62 @@
 const commands = require('commands')
 const axios = require('./axios');
 const {ImageFill, Color} = require("scenegraph");
-const endPoint = "https://data-uri-imagefill.herokuapp.com/?image=https://via.placeholder.com/"
+const endPoint = "https://data-uri-imagefill.herokuapp.com/?image=https://via.placeholder.com/";
+const { alert, error } = require('./lib/dialogs.js');
+
 
 const runImages = async (selection) => {
   let types = ["Rectangle", "Ellipse", "Polygon"];
-  for(let i = 0; i < selection.items.length; i++){
-    let selected = selection.items[i];
-    let { localBounds } = selection.items[i];
-    
-    // if color of placeholder decides to get added back
-    // let activeFill = new Color(selected.fill.value, 1).toHex().split("#")[1];
-    // let addColor = activeFill === "fff" ? "CCCCCC" : activeFill;
-    // let addedColor = addColor;
-    
-    if(types.includes(selected.constructor.name)) {
-      let height = Math.round(localBounds.height);
-      let width = Math.round(localBounds.width);
-      let getDate = await fetchImage(height, width);
-      let fillImage = await new ImageFill(getDate)
-      selected.fill = fillImage;
+  if(selection.items.length >= 1 && selection.items[0].constructor.name != "Artboard") {
+    for(let i = 0; i < selection.items.length; i++){
+      let selected = selection.items[i];
+      let { localBounds } = selection.items[i];
+      
+      if(types.includes(selected.constructor.name)) {
+        let height = Math.round(localBounds.height);
+        let width = Math.round(localBounds.width);
+        let getDate = await fetchImage(height, width);
+        let fillImage = await new ImageFill(getDate)
+        selected.fill = fillImage;
+      }
+  
     }
-
+  } else {
+    alertUserNoObj()
   }
-
 }
 
 const fetchImage = (height, width) => {
   return axios(`${endPoint}${width}x${height}.png/?text=${width}x${height}`)
-  .then(response =>  response.data)
-  .catch((err) => console.log(err))
+  .then(response =>  {
+    return response.data
+  })
+  .catch((err) => {
+    console.log(err)
+    noConnection();
+  })
 }
+
+
+
+const alertUserNoObj = async () => {
+  await error(
+    "Placeholder Image error",
+    "Please select one or more of the following: ",
+    "* Rectangle",
+    "* Ellipse",
+    "* Polygon"
+  );
+}
+
+const noConnection = async () => {
+  await error(
+    "Placeholder Image error",
+    "Please connect to the internet and try again ",
+  );
+}
+
+
 
 module.exports = {
   commands: {
